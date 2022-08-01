a2701cc7d2daa1e9104c421500c6dba8fd56ee01
Roger-Franco <rogerfrancogalt@gmail.com>
2022-07-31 22:42:12 -0400

    Propagando "props" para componentes filho #02

--- src/componentes/Familia.jsx
+++ src/componentes/Familia.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
+import { filhosComProps } from '../utils/utils'
 
 export default props =>
   <div>
     <h1>Família:</h1>
-    {React.cloneElement(props.children, { ...props })}
-    {/* {React.cloneElement(props.children, props)}
-    {React.cloneElement(props.children, { sobrenome: props.sobrenome })} */}
-    {/* {props.children} */}
-    {/* {props.sobrenome} */}
+
+    {filhosComProps(props)}
+
+    {/* {React.Children.map(props.children, filho => {
+      return React.cloneElement(filho, { ...props })
+    })} */}
+    {/* {React.cloneElement(props.children, { ...props })} */}
+    {/* {React.cloneElement(props.children, props)} */}
+    {/* {React.cloneElement(props.children, { sobrenome: props.sobrenome })}
+    {props.children}
+    {props.sobrenome} */}
   </div>
